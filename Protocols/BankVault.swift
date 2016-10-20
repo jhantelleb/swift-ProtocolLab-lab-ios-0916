d51bdc268f894cb141200c5d8f82e3d27cc6b38a
Jhantelle Belleza <jhantellebelleza@Jhantelles-MacBook-Pro.local>
2016-10-20 16:40:06 -0400

    Completed Exercise

--- Protocols/BankVault.swift
+++ Protocols/BankVault.swift
@@ -17,9 +17,23 @@ class BankVault {
         self.name = name
         self.address = address
     }
-    
 }
 
+protocol ProvideAccess {
+    func allowEntryWithPassword(_ password: [Int]) -> Bool
+}
+
+extension BankVault: ProvideAccess {
+    func allowEntryWithPassword(_ password: [Int]) -> Bool {
+        if password.count > 10 || password.isEmpty {
+            return false
+        } else {
+            for (index, value) in password.enumerated() {
+                if index % 2 == 0 && value % 2 == 0 { return true
+                } else { return false }}
+        }
+    return false }
+}
 
 
 
